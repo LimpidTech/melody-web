7b59ad17e5266c00709b448708a94e8a69065c20
Bailey Stoner <monokrome@monokro.me>
2017-12-26 01:42:12 -0800

    Add a header

--- src/components/application/index.jsx
+++ src/components/application/index.jsx
@@ -1,4 +1,20 @@
-import Header from 'components/header'
 import React from 'react'
 
-export default () => <Header />
+import styled from 'styled-components'
+
+const Application = props => (
+  <section className={props.className}>
+    <header>
+      <h1>Melody</h1>
+    </header>
+
+    <main></main>
+
+    <footer></footer>
+  </section>
+)
+
+export default styled(Application)`
+  padding: 0.4rem 0.8rem 0.1rem;
+  background-color: #ECECFE;
+`
