57ce49fd3c594fc0ed4de71f2b2a83136cd7f65c
Bailey Stoner <monokrome@monokro.me>
2018-01-19 02:46:49 -0800

    Fix home link

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 
 export default () => (
   <nav>
-    <Link to='/home/'>Home</Link>
+    <Link to='/'>Home</Link>
     <Link to='/categories/'>Categories</Link>
     <Link to='/login/'>Login</Link>
     <Search />
