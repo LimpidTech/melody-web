5f6221b6287236bc886e48df00ecdda5320b5c60
Bailey Stoner <monokrome@monokro.me>
2018-02-23 02:06:21 -0800

    Update layout to match old style with UX focus.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,125 @@
 <template>
-  <div>
-    <nuxt/>
-  </div>
+  <section>
+    <header>
+      <h2>Melody</h2>
+
+      <nav>
+        <a>home</a>
+        <a>recent</a>
+        <a>account</a>
+      </nav>
+    </header>
+
+    <main>
+      <nuxt/>
+    </main>
+
+    <footer>
+      <a href='https://github.com/MelodyBoard'>Melody</a>
+    </footer>
+  </section>
 </template>
 
 <style>
-html
-{
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-*, *:before, *:after
-{
-  box-sizing: border-box;
-  margin: 0;
-}
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
-}
+  body {
+    font-family: sans-serif;
+    font-size: 1.35em;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  #__layout * {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  #__layout * ~ * {
+    margin-top: 0.4em;
+  }
+
+  #__layout > section {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+
+  #__layout > section > header {
+    padding: 0.4rem 0.8rem 0;
+    user-select: none;
+  }
+
+  #__layout > section > header > h2 {
+    display: none;
+    margin-top: 0;
+    margin-right: 3rem;
+  }
+
+  #__layout > section > header > nav > a {
+    display: block;
+    color: #393939;
+    text-decoration: none;
+  }
+
+  #__layout > section > header > nav > input {
+    display: block;
+    margin-top: 3em;
+  }
+
+  #__layout > section > main {
+    flex-grow: 1;
+    padding: 0.4em;
+    margin-bottom: 0;
+  }
+
+  #__layout > section > footer {
+    display: none;
+    font-size: 1rem;
+    text-align: center;
+    user-select: none;
+  }
+
+  @media (min-width: 780px) {
+    #__layout > section > section {
+      flex-direction: column;
+    }
+
+    #__layout > section > header {
+      height: 2em;
+      overflow: visible;
+    }
+
+    #__layout > section > header > h2 {
+      display: inline-block;
+    }
+
+    #__layout > section > header > nav {
+      float: right;
+      display: inline-block;
+      margin-top: 0;
+    }
+
+    #__layout > section > header > nav > a {
+      display: inline-block;
+      padding-right: 3em;
+      padding-bottom: 0.2em;
+      font-size: 0.9em;
+      vertical-align: bottom;
+    }
+
+    #__layout > section > header > nav > input {
+      display: inline-block;
+      margin-top: 0;
+    }
+
+    #__layout > section > main {
+      max-width: 960px;
+      margin: 0 auto;
+    }
+
+    #__layout > section > footer {
+      display: block;
+      margin-top: 6rem;
+      min-height: 3rem;
+    }
+  }
 </style>
