60444ec5d2a8b0014ad4eb09464d8b9e1e0af3bc
Bailey Stoner <monokrome@monokro.me>
2018-07-01 11:40:25 -0700

    Add a simplke "account" page.

---
+++ pages/account/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{ user.username }}</h2>
+</template>
+
+<script>
+import page from '~/helpers/pages'
+
+export default page({
+  computed: {
+    user() { return this.$store.state.user },
+  },
+})
+</script>
